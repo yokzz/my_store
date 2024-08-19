29bee76ac589fa45a346ce503a9b0a89d0ca8cff
Zhelizko Daniel <yokzz.programming@gmail.com>
2024-08-19 11:58:01 +0300

    added reviews for products

--- static/js/function.js
+++ static/js/function.js
@@ -1,23 +1,61 @@
-console.log('Working fine')
+console.log('Working fine');
+
+const monthNames = ["Jan", "Feb", "Mar", "April",
+    "May", "June", "July", "Aug", "Sept", "Oct",
+    "Nov", "Dec"
+];
+
 
 $("#reviewForm").submit(function(event){
     event.preventDefault();
 
+    let dt = new Date();
+    let time = dt.getDay() + " " + monthNames[dt.getUTCMonth()] + ", " + dt.getFullYear()
+
     $.ajax({
         data: $(this).serialize(),
-
         method: $(this).attr("method"),
-
         url: $(this).attr("action"),
-
         dataType: "json",
-
         success: function(response){
             console.log('Review saved to DB...');
-        
+            
             if (response.bool == true){
-                $("#review-response").html("Review added successfully")
+                $("#review-response").html("Review added successfully");
+                $(".hide-review").hide();
+                $(".hide-review-form").hide();
+
+
+                let _html = '<!-- Single Review -->';
+                    _html += '<div class="single-review">'
+                    _html += '<!-- Review User Logo & Username -->';
+                    _html += '<div class="review-header">';
+                    _html += '<img src='+ adminLogoUrl +'alt="User Avatar" class="review-avatar" />';
+                    _html += '<a href="#" class="review-username">'+ response.context.user +'</a>';
+                    _html += '</div>';
+
+                    _html += '<!-- Review Content -->';
+                    _html += '<div class="review-content">';
+                    _html += '<p>'+ response.context.review  +'</p>';
+                    _html += '</div>';
+
+                    _html += '<!-- Review Date -->';
+                    _html += '<div class="review-date">';
+                    _html += '<span>'+ time +'</span>';
+                    _html += '</div>';
+
+                    _html += '<!-- Review Rating -->';
+                    _html += '<div class="review-rating">';
+
+                    for(let i = 1; i <= response.context.rating; i++){
+                        _html += '<span>★</span>';
+                    }
+
+                    _html += '</div>';
+                    _html += '</div>';
+
+                    $(".reviews").prepend(_html); 
             }
-        },
-    })
+        }
+    });
 });
--- store/views.py
+++ store/views.py
@@ -37,7 +37,15 @@ def product_detail_view(request, pid):
 
     # Product review form
     review_form = ProductReviewForm()
+    
+    make_review = True 
+    
+    if request.user.is_authenticated:
+        user_review_count = ProductReview.objects.filter(user=request.user, product=product).count()
 
+        if user_review_count > 0:
+            make_review = False
+            
     context = {
         "product": product,
         "products": products,
@@ -45,6 +53,7 @@ def product_detail_view(request, pid):
         "reviews": reviews,
         "review_form": review_form,
         'average_rating': average_rating,
+        "make_review": make_review,
     }
     
     return render(request, "store/product-detail.html", context)
